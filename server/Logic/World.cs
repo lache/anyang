1b7626c19e8a2fa2511ff3bfdc38c611f1ffd1e4
lacti <lactrious@gmail.com>
2014-02-17 02:04:58 +0900

    이동 불가 지역 구현
    * tiled 맵에서 읽은 장애물 정보를 토대로 이동 가능/불가 여부를 확인할 수 있도록 구현
    * 일단 대충 default_1.tmx만 읽어서 하나의 obstacles 배열만 처리할 수 있도록 구현

--- server/Logic/World.cs
+++ server/Logic/World.cs
@@ -1,6 +1,7 @@
 ﻿using Server.Core;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -14,11 +15,23 @@ namespace Server.Logic
     {
         private readonly Coroutine _coro;
         private readonly Persistence _persistence;
+        private readonly TiledSharp.TmxMap _map;
+        private readonly bool[,] _obstacles;
 
         public World(Coroutine coro, Persistence persistence)
         {
             _coro = coro;
             _persistence = persistence;
+            _map = new TiledSharp.TmxMap(Path.Combine("Data", "default_1.tmx"));
+
+            _obstacles = new bool[_map.Width, _map.Height];
+            var obstacleGids = _map.Tilesets.First().Tiles.Where(e => e.Properties.ContainsKey("type") && (e.Properties["type"] == "wall" || e.Properties["type"] == "obstacle"))
+                .Select(e => e.Id).ToList();
+            var obstacles = _map.Layers.First().Tiles.Where(e => obstacleGids.Contains(e.Gid)).Select(e => Tuple.Create(e.X, e.Y));
+            foreach (var tuple in obstacles)
+            {
+                _obstacles[tuple.Item1, tuple.Item2] = true;
+            }
         }
 
         public IEnumerable<int> CoroEntry()
@@ -32,5 +45,12 @@ namespace Server.Logic
 
         public Coroutine Coro { get { return _coro; } }
         public Persistence Persist { get { return _persistence; } }
+
+        public bool CanMove(int x, int y)
+        {
+            if (x < 0 || x >= _obstacles.GetLength(0)) return false;
+            if (y < 0 || y >= _obstacles.GetLength(1)) return false;
+            return !_obstacles[x, y];
+        }
     }
 }
