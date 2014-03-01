e10a068971cef127bd0a8728cc0987fcb1caa055
lacti <lactrious@gmail.com>
2014-03-01 17:36:11 +0900

    서버가 50ms마다 위치를 보내주도록 수정

--- server/Logic/Player.cs
+++ server/Logic/Player.cs
@@ -36,6 +36,7 @@ namespace Server.Logic
     {
         private readonly Extrapolator _polator = new Extrapolator();
         private PlayerData _data = new PlayerData { Name = "nonamed" };
+        private bool _logged;
 
         public Player(World world, Session session)
             : base(world, session)
@@ -53,6 +54,7 @@ namespace Server.Logic
             _world.Actors.Add(this);
             _polator.Reset(0, 0, new Vector2 { X = _data.X, Y = _data.Y });
 
+            _logged = true;
             Logger.Write("{0} is logged.", msg.Name);
 
             // WorldInfo의 Spawn 패킷에는 자기 자신에 대한 정보를 보내지 않는다.
@@ -75,20 +77,23 @@ namespace Server.Logic
                 new UpdateHpMsg(_data.ObjectId, _data.MaxHp, _data.Hp));
         }
 
+        private void MovePosition(double x, double y, double dir, double speed)
+        {
+            _data.X = x;
+            _data.Y = y;
+            _data.Dir = dir;
+            _data.Speed = speed;
+
+            var posMsg = new UpdatePositionMsg(_data.ObjectId, x, y, speed, dir, Realtime.Now, false);
+            foreach (var actor in _world.GetActors<NetworkActor>())
+                actor.SendToNetwork(posMsg);
+        }
+
         void OnMove(MoveMsg msg)
         {
-            _data.X = msg.X;
-            _data.Y = msg.Y;
-            _data.Dir = msg.Dir;
-            _data.Speed = msg.Speed;
-
             _polator.AddSample(msg.Time, Realtime.Now, 
                 new Vector2 { X = msg.X, Y = msg.Y },
                 new Vector2 { X = msg.Speed * Math.Cos(msg.Dir), Y = msg.Speed * Math.Sin(msg.Dir) });
-
-            var posMsg = new UpdatePositionMsg(msg.Id, msg.X, msg.Y, msg.Speed, msg.Dir, msg.Time, false);
-            foreach (var actor in _world.GetActors<NetworkActor>())
-                actor.SendToNetwork(posMsg);
         }
 
         void OnChat(ChatMsg msg)
@@ -103,11 +108,26 @@ namespace Server.Logic
             yield return 1000;
             _world.Persist.Store(_data);
             _world.Actors.Remove(this);
+            _logged = false;
             Logger.Write("{0} is logout.", _data.Name);
 
             var msg = new DespawnMsg { Id = _data.ObjectId };
             foreach (var actor in _world.Actors.OfType<NetworkActor>())
                 actor.SendToNetwork(msg);
+        }
+
+        private IEnumerable<int> CoroUpdatePos()
+        {
+            while (_logged)
+            {
+                Vector2 pos, velocity;
+                _polator.ReadPosition(Realtime.Now, out pos, out velocity);
+
+                MovePosition(pos.X, pos.Y, _data.Dir, _data.Speed);
+
+                const int sendCount = 20;
+                yield return 1000 / sendCount;
+            }
         }
     }
 }
