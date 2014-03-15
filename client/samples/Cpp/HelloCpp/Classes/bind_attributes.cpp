f3e2154ba29aa318edd4b957e81a7365f51fa1c7
Geoyeob Kim <gasbank@gmail.com>
2014-03-16 01:51:52 +0900

    msg 자동 생성 파일 추가

---
+++ client/samples/Cpp/HelloCpp/Classes/bind_attributes.cpp
@@ -0,0 +1,17 @@
+﻿/* this code is auto-generated. */
+
+#include ""
+#include "singleton.h"
+#include "cbes/attribute.h"
+
+#pragma warning (disable: 4189 4100 4505)
+
+
+#pragma region /* factory-register */
+static struct __initializer1 {
+    __initializer1()
+    {
+    
+    }
+} ___init2;
+#pragma endregion
---
+++ client/samples/Cpp/HelloCpp/Classes/data_center_client.cpp
@@ -0,0 +1,20 @@
+﻿/* this code is auto-generated. */
+#include <clientpch.h>
+#include <data_expression.h>
+#include <data_center.h>
+
+#pragma warning( disable : 4996 )
+
+using namespace data;
+
+
+
+
+static struct __initializer100 {
+    __initializer100()
+    {
+        
+    }
+} ___init101;
+
+#pragma warning( default : 4996 )
