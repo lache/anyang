ccfe91bc7a4deedd87ce45f7c5a656966e4cf629
lacti <lactrious@gmail.com>
2014-03-22 15:39:18 +0900

    서버측 UnitTest 기반 추가
    * 예제 테스트 코드 추가

---
+++ server/Test/CoreTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Server.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Test
+{
+    [TestFixture]
+    class CoreTest
+    {
+        [Test]
+        public void ConnectionTest()
+        {
+            const int port = 29321;
+            var network = new Network();
+            network.StartServer(port + 1);
+            var peerSession = network.Connect("127.0.0.1", port);
+            peerSession.Disconnect();
+        }
+    }
+}
